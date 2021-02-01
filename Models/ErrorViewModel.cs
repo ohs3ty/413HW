cc4ddcd788cf6ff5bbec935e7ba48d4035fb27ee
ohs3ty <59738287+ohs3ty@users.noreply.github.com>
2021-02-01 16:26:34 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HW3_413.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
